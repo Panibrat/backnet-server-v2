46747b7e044f8e0e95605778ee5199922d1b1138
A.Panibratenko <a.panibratenko@gmail.com>
2018-08-28 23:58:12 +0300

    x-auth token verify.

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,12 +1,14 @@
+const jwt = require('jsonwebtoken');
+
 const authenticate = (req, res, next) => {
     const token = req.header('x-auth');
-
-    if (token === 'token') {
-        req.token = token;
-        next();
-    } else {
+    try {
+        const decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
         res.status(401).send();
     }
+        req.token = token;
+        next();
 };
 
 module.exports = { authenticate };
