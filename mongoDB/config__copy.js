1147effbc9441b4be0d1c1bb79b4210d2bd28ec7
A.Panibratenko <a.panibratenko@gmail.com>
2018-09-04 23:11:44 +0300

    some public files added + sample of Mongo config

---
+++ mongoDB/config__copy.js
@@ -0,0 +1,17 @@
+const DATABASE_HOST = 'your_value.mlab.com';
+const DATABASE_PORT = 0;
+const DATABASE_NAME = 'your_value';
+const DATABASE_TREND_NAME = 'trendsCollection';
+const MAX_TREND_COLLECTION_SIZE = 100000000;
+const DATABASE_USER_NAME = 'your_value';
+const DATABASE_USER_PASSWORD = 'your_value';
+
+module.exports = {
+    DATABASE_USER_NAME,
+    DATABASE_USER_PASSWORD,
+    DATABASE_HOST,
+    DATABASE_PORT,
+    DATABASE_NAME,
+    DATABASE_TREND_NAME,
+    MAX_TREND_COLLECTION_SIZE,
+};
