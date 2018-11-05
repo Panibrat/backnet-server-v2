411ee23cc2f3576f536e494032ede1d09415f932
Panibrat <a.panibratenko@gmail.com>
2018-11-06 01:11:47 +0200

    Modbus Loop run to console.

--- modbus/modbusLoop.js
+++ modbus/modbusLoop.js
@@ -1,4 +1,7 @@
-const ModbusRTU = require("modbus-serial");
+const ModbusRTU = require('modbus-serial');
+const modbusTable = require('./modbusRegisterTable.json');
+const { getBufferFromTable } = require('./helpers');
+
 const modbusClient = new ModbusRTU();
 
 const MODBUS_IP = '192.168.1.155';
@@ -7,39 +10,18 @@ const MODBUS_PORT = 502;
 const POLING_TIME = 1000;
 const RECONNECT_TIME = 5000;
 
-/*modbusClient
-    .connectTelnet(MODBUS_IP, { port: MODBUS_PORT })
-    .then(() => {
-        modbusClient.setID(MODBUS_ID)
-    })
-    .then(() => {
-        console.log('loop read Promise All');
-        run();
-    })
-    .catch((e) => {
-        console.log('[MODBUS_ERROR: ]', e);
-        modbusClient.close();
-        console.log('wait and run again');
-        setTimeout(() => {
-            run();
-        }, RECONNECT_TIME);
-    });*/
-
-function run() {
-    const stop = setInterval(() => {
-        console.log('read');
-    }, POLING_TIME)
-}
+const initBuffer = getBufferFromTable(modbusTable);
 
 class ModbusLoop {
-    constructor(client, ip, port, id) {
-        this.buffer = [];
+    constructor(client, ip, port, id, buffer) {
+        this.buffer = buffer;
         this.client = client;
         this.ip = ip;
         this.port = port;
         this.id = id;
         this.stop = null;
         this.connect = this.connect.bind(this);
+        this.readPoint = this.readPoint.bind(this);
     }
 
     connect() {
@@ -57,35 +39,73 @@ class ModbusLoop {
                 this.client.close();
                 clearInterval(this.stop);
                 setTimeout(this.connect, RECONNECT_TIME);
-        });
+            });
+    }
+
+    getBuffer() {
+        return this.buffer;
     }
 
     getIntBE8Bytes(registerAddress) {
         return this.client.readHoldingRegisters(registerAddress, 8)
-            .then( (data) => {
-                return data.buffer.readIntBE(2, 6);
-            });
+            .then((data) => {
+                return data.buffer.readIntBE(0, 8);
+            })
+            .catch(e => console.log(e));
     }
 
     getFloatBE(registerAddress) {
         return this.client.readHoldingRegisters(registerAddress, 2)
-            .then( (data) => {
+            .then((data) => {
                 return data.buffer.readFloatBE();
-            });
+            })
+            .catch(e => console.log(e));
+    }
+
+    readPoint(point) {
+        switch (point.type) {
+        case 'FloatBE':
+            return () => {
+                return this.getFloatBE(point.address)
+                    .then((value) => {
+                        return {
+                            name: point.name,
+                            value: value,
+                        };
+                    });
+            };
+        case 'IntBE':
+            return () => {
+                return this.getIntBE8Bytes(point.address)
+                    .then((value) => {
+                        return {
+                            name: point.name,
+                            value: value,
+                        };
+                    });
+            };
+        default:
+            return null;
+        }
     }
 
     runLoop() {
+        const arrTask = Object.keys(this.buffer).map((point) => {
+            return this.readPoint(this.buffer[point]);
+        });
         this.stop = setInterval(() => {
-            this.getIntBE8Bytes(3203).then((value) => console.log('EnergyTotal', value));
-            this.getIntBE8Bytes(4195).then((value) => console.log('EnergyDayTotal', value));
-            this.getIntBE8Bytes(4199).then((value) => console.log('EnergyNightTotal', value));
-            this.getFloatBE(2999).then((value) => console.log('I1', value));
-            this.getFloatBE(3027).then((value) => console.log('L1N', value));
-            this.getFloatBE(3053).then((value) => console.log('Pwr1', value));
-
+            console.log(this.buffer);
+            arrTask.reduce((sum, currentPromise) => {
+                return sum
+                    .then(currentPromise)
+                    .then((data) => {
+                        this.buffer[data.name] = data.value;
+                    })
+                    .catch(e => console.log(e));
+            }, Promise.resolve([]));
         }, POLING_TIME);
     }
 }
 
-const loop = new ModbusLoop(modbusClient, MODBUS_IP, MODBUS_PORT, MODBUS_ID);
+const loop = new ModbusLoop(modbusClient, MODBUS_IP, MODBUS_PORT, MODBUS_ID, initBuffer);
 loop.connect();
