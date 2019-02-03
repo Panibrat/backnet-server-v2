06b231c9d7cda9a65982321ee9bd341f7ca623ac
A.Panibratenko <a.panibratenko@gmail.com>
2019-02-03 22:24:04 +0200

    Floors View Pages: Button floors switcher added

--- src/client/components/Pages/PlansPage/PlansPage.jsx
+++ src/client/components/Pages/PlansPage/PlansPage.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Button from '@material-ui/core/Button';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import FirstFloorItem  from '../../FirstFloorItem';
@@ -7,14 +8,34 @@ import { setTitle } from '../../../actions/menuActions';
 import styles from './PlansPage.css';
 
 class PlansPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFirstFloorShown: true
+        };
+
+        this.toggleFloor = this.toggleFloor.bind(this);
+    }
+
     componentDidMount() {
         this.props.setTitle('Планы этажей');
     }
+
+    toggleFloor() {
+        const title = this.state.isFirstFloorShown ? 'Второй этаж' : 'Первый этаж';
+        this.props.setTitle(title);
+        this.setState({
+            isFirstFloorShown: !this.state.isFirstFloorShown
+        })
+    }
+
     render() {
         return (
             <div className={styles.container}>
-                <FirstFloorItem />
-                <SecondFloorItem />
+                {this.state.isFirstFloorShown ? <FirstFloorItem /> : <SecondFloorItem />}
+                <Button variant="contained" color="primary" onClick={this.toggleFloor}>
+                    {this.state.isFirstFloorShown ? 'Второй этаж' : 'Первый этаж'}
+                </Button>
             </div>
         );
     }
