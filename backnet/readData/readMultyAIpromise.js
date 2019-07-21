2d3812b9d67fe0335262110e4560a82fcd90fbab
A.Panibratenko <a.panibratenko@gmail.com>
2019-07-22 01:49:11 +0300

    [TEST] BACnet: readPropertyMultiple. Initial: 2-4

--- backnet/readData/readMultyAIpromise.js
+++ backnet/readData/readMultyAIpromise.js
@@ -15,15 +15,24 @@ const readMultyAI = (arr) => {
                 console.log('error1----->\n', err);
             } else {
                 // console.log('value----->\n', value);
+/*
                 console.log('value.values----->\n', value.values);
                 console.log('value.values[0]----->\n', value.values[0]);
                 console.log('value.values[0].objectIdentifier----->\n', value.values[0].objectIdentifier);
                 console.log('value.values[0].values[0]----->\n', value.values[0].values[0]);
+*/
                 //console.log('value.values[1].values[0]----->\n', value.values[1].values[0]);
                 if (value) {
                     value.values.forEach((item, i) => {
-                        console.log( 'objectIdentifier', value.values[i].objectIdentifier);
-                        console.log( i + ' -----> \n', value.values[i].values[0]);
+/*
+                        console.log( 'objectIdentifier', value.values[i].objectIdentifier.instance);
+                        console.log( i + ' -----> \n', value.values[i].values[0].value[0].value);
+*/
+                        const resolveItem = {
+                            title: `AI${value.values[i].objectIdentifier.instance}`,
+                            value: value.values[i].values[0].value[0].value,
+                        };
+                        console.log('resolveItem', resolveItem);
                     })
                 }
             }
