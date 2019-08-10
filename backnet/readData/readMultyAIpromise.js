194dfdeaf9a622e42bc7cc78c39bb93b7d3175f3
A.Panibratenko <a.panibratenko@gmail.com>
2019-08-10 23:48:05 +0300

    [TEST] Multy AI Read: v-006

--- backnet/readData/readMultyAIpromise.js
+++ backnet/readData/readMultyAIpromise.js
@@ -11,23 +11,12 @@ const readMultyAI = (arr) => {
         BACnetClient.ip, // IP device
         arr,
         (err, value) => {
+            const startTime = new Date().getTime();
             if (err) {
                 console.log('error1----->\n', err);
             } else {
-                // console.log('value----->\n', value);
-/*
-                console.log('value.values----->\n', value.values);
-                console.log('value.values[0]----->\n', value.values[0]);
-                console.log('value.values[0].objectIdentifier----->\n', value.values[0].objectIdentifier);
-                console.log('value.values[0].values[0]----->\n', value.values[0].values[0]);
-*/
-                //console.log('value.values[1].values[0]----->\n', value.values[1].values[0]);
                 if (value && value.values) {
                     value.values.forEach((item, i) => {
-/*
-                        console.log( 'objectIdentifier', value.values[i].objectIdentifier.instance);
-                        console.log( i + ' -----> \n', value.values[i].values[0].value[0].value);
-*/
                         if (
                             value &&
                             value.values &&
@@ -43,15 +32,17 @@ const readMultyAI = (arr) => {
                                 title: `AI${value.values[i].objectIdentifier.instance}`,
                                 value: +value.values[i].values[0].value[0].value.toFixed(1)
                             };
-                            console.log('resolveItem', resolveItem);
+                            // console.log('resolveItem', resolveItem);
                             buffer.setData(resolveItem);
                         } else {
-                            console.log(`[readMultyAI] AI${value.values[i].objectIdentifier.instance}`);
+                            console.log(`[error readMultyAI] AI${value.values[i].objectIdentifier.instance}`);
                         }
                     })
                 } else {
                     console.log('[readMultyAI] no data ');
                 }
+                const stopTime = new Date().getTime();
+                console.log('running Time:', stopTime - startTime);
             }
         },
     );
