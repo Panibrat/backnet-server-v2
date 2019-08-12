c7c6c5ea7988220b81cecb1c22c469d01a4c6820
A.Panibratenko <a.panibratenko@gmail.com>
2019-08-13 00:16:37 +0300

    up AI + AO polling time to 10000

--- backnet/BACnetLoop.js
+++ backnet/BACnetLoop.js
@@ -74,10 +74,10 @@ class BACnetLoop {
     }
 
     run() {
-        // this.runMultyAI();
-        // this.runMultyAO();
+        this.runMultyAI();
+        this.runMultyAO();
         this.runMultyBO();
-        //this.runMultyBI();
+        this.runMultyBI();
 
         // this.runAI();
         // this.runAO();
@@ -173,7 +173,7 @@ class BACnetLoop {
     }
 
     runMultyAI() {
-        setInterval(() => readMultyAI(objectsAiArray), pollingTime * 3);
+        setInterval(() => readMultyAI(objectsAiArray), pollingTime * 5);
     }
 
     runMultyAO() {
@@ -181,11 +181,11 @@ class BACnetLoop {
     }
 
     runMultyBO() {
-        setInterval(() => readMultyBO(objectsBOArray), pollingTime * 0.5);
+        setInterval(() => readMultyBO(objectsBOArray), pollingTime);
     }
 
     runMultyBI() {
-        setInterval(() => readMultyBI(objectsBIArray), pollingTime * 3);
+        setInterval(() => readMultyBI(objectsBIArray), pollingTime * 5);
     }
 }
 
