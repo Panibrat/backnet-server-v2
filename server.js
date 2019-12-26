1fef49675175ef5a6a48f3856048dc58bbf29623
Oleksandr_Panibraten <oleksandr_panibratenko@epam.com>
2019-12-26 11:59:24 +0200

    [test] HP Elite upload

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ mongoose.connect(dataBaseLink, {
 })
     .then(() => {
         console.log('connected to MongoDB');
+        console.log('HP Elite G6 test upload');
     })
     .catch((e) => {
         console.log('[ERROR] with connection to MongoDB: ', e);
